f59c2f5676f6ad6da91c6d762c555ec748d503e3
Aaron Tinio <aptinio@gmail.com>
2015-04-04 23:28:38 +0800

    Put functions inside the closure.
    Closes #5.

--- index.js
+++ index.js
@@ -46,51 +46,51 @@ module.exports = function (apiKey) {
     )
 
     setValueOfWonOpportunitiesThisMonthByUser()
-  }
 
-  function query(key, path, query, prop) {
-    closeio._get(path, { _fields: '', query: query })
-      .then(function (results) {
-        metrics.set(key, results[prop])
-      }, debugError)
-  }
-
-  function setValueOfWonOpportunitiesThisMonthByUser() {
-    var date = new Date()
-    date.setDate(1)
-    var since = date.toISOString().split('T')[0]
-    var users = {}
-
-    aggregateWonOpportunityValues(0)
-    metrics.set('closeio won opportunities value this month by user', users)
-
-    function aggregateWonOpportunityValues(lastCount) {
-      closeio
-        ._get('/opportunity/', {
-          _fields: 'user_id,user_name,value',
-          _skip: lastCount,
-          query: 'status_type: won and date >= ' + since
-        })
+    function query(key, path, query, prop) {
+      closeio._get(path, { _fields: '', query: query })
         .then(function (results) {
-          lastCount += results.data.length
-
-          results.data.forEach(function (data) {
-            var user = users[data.user_id] = users[data.user_id] || {
-              name: data.user_name,
-              value: 0
-            }
-
-            user.value += data.value
-          })
-
-          if (results.total_results > lastCount) {
-            aggregateWonOpportunityValues(lastCount)
-          }
+          metrics.set(key, results[prop])
         }, debugError)
     }
-  }
 
-  function debugError(error) {
-    debug('Error: %s', error)
+    function setValueOfWonOpportunitiesThisMonthByUser() {
+      var date = new Date()
+      date.setDate(1)
+      var since = date.toISOString().split('T')[0]
+      var users = {}
+
+      aggregateWonOpportunityValues(0)
+      metrics.set('closeio won opportunities value this month by user', users)
+
+      function aggregateWonOpportunityValues(lastCount) {
+        closeio
+          ._get('/opportunity/', {
+            _fields: 'user_id,user_name,value',
+            _skip: lastCount,
+            query: 'status_type: won and date >= ' + since
+          })
+          .then(function (results) {
+            lastCount += results.data.length
+
+            results.data.forEach(function (data) {
+              var user = users[data.user_id] = users[data.user_id] || {
+                name: data.user_name,
+                value: 0
+              }
+
+              user.value += data.value
+            })
+
+            if (results.total_results > lastCount) {
+              aggregateWonOpportunityValues(lastCount)
+            }
+          }, debugError)
+      }
+    }
+
+    function debugError(error) {
+      debug('Error: %s', error)
+    }
   }
 }
